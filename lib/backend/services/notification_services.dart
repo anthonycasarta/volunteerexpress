99c67137ce590fd9d00071d9dc90e2d4226ef3b4
alessandrojm1 <alessandro061603@gmail.com>
2024-11-15 12:32:43 -0600

    updated collection's parameters/fields

--- lib/backend/services/notification_services.dart
+++ lib/backend/services/notification_services.dart
@@ -12,17 +12,19 @@ class NotificationServices {
           'title': doc['title'],
           'description': doc['description'],
           'time': doc['time'].toDate(),
+          'requires_action': doc['requires_action'],
         };
       }).toList();
 
       return notifications;
   }
 
-  Future<void> addNotification(String title, String description, DateTime time) async {
+  Future<void> addNotification(String title, String description, DateTime time, bool requiresAction) async {
       await firestore.collection('NOTIFICATION').add({
         'title': title,
         'description': description,
         'time': Timestamp.fromDate(time),
+        'requires_action': requiresAction,
       });
   }
 }
